1f673fa18215ddfc21a7c6843ae0c9e7fec76e59
Darnell <darnjames@gmail.com>
2025-01-08 23:45:55 -0500

    Add nav component to dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,3 +1,26 @@
+import { DashNav } from '../../components/DashNav/DashNav';
+import { useState } from 'react';
+
 export function Dashboard() {
-  return <h1>Dashboard page</h1>;
+  const [showSideNav, setShowSideNav] = useState(false);
+
+  const showNav = () => setShowSideNav(true);
+  const hideNav = () => setShowSideNav(false);
+  // header, main, footer
+  // header will contain a nav
+  // there will be another nav that acts as side nav options
+  // main content holds the post list and side nav, which overflows.
+  // should there even be a footer
+  // the footer should go underneath the main. Sure. Why not
+  return (
+    <>
+      <header>
+        <DashNav showNav={showNav} />
+      </header>
+      {/* Put the side nav in main so I can put them in a flex box or something */}
+      <nav>Side nav stuff</nav>
+      <main>Post list, with post options on desktop</main>
+      <footer>Footer stuff</footer>
+    </>
+  );
 }
