456cbfd2b2cfc250152529e3858349269e52eab7
Darnell <darnjames@gmail.com>
2025-01-15 18:57:39 -0500

    Reorganize html

--- src/components/DashNav/DashNav.jsx
+++ src/components/DashNav/DashNav.jsx
@@ -1,101 +1,33 @@
 import Hamburger from '../../assets/icons/hamburger.svg?react';
-import Search from '../../assets/icons/search.svg?react';
-import { Link, NavLink } from 'react-router';
-import { useId } from 'react';
+import { Link } from 'react-router';
+import { SearchFrom } from '../SearchForm/SearchForm';
 import styles from './DashNav.module.css';
 import PropTypes from 'prop-types';
+import { HeaderLinks } from '../HeaderLinks/HeaderLinks';
 
 export function DashNav({ showNav }) {
-  const id = useId();
-
   return (
-    <nav>
-      <ul className={styles.navFlex1}>
-        <li className={styles.listItemBtn}>
-          <button onClick={showNav} className={styles.navHamburger}>
-            <Hamburger />
-          </button>
-        </li>
-        <li>
-          <Link
-            onClick={() => window.location.reload()}
-            to={'/'}
-            className={styles.logoLink}
-          >
-            Rumblr
-          </Link>
-        </li>
-        <li>
-          <ul className={styles.ulNested}>
-            <li>
-              <NavLink
-                to={'/'}
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.navLink} ${styles.navLink__active}`
-                    : styles.navLink
-                }
-              >
-                Following
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={'/likes'}
-                className={({ isActive }) =>
-                  isActive
-                    ? `${styles.navLink} ${styles.navLink__active}`
-                    : styles.navLink
-                }
-              >
-                Likes
-              </NavLink>
-            </li>
-          </ul>
-        </li>
+    <div className={styles.topBar}>
+      <button onClick={showNav} className={styles.menuBtn}>
+        <Hamburger />
+      </button>
 
-        <li className={styles.formListItem}>
-          <search>
-            <form className={styles.searchForm}>
-              <label
-                aria-labelledby="searchTag"
-                htmlFor={id}
-                className={styles.searchLabel}
-              >
-                <Search title="searchTag" />
-              </label>
-              <input className={styles.searchInput} id={id} type="search" />
-            </form>
-          </search>
-        </li>
-      </ul>
-      <ul className={styles.navFlex2}>
-        <li>
-          <NavLink
-            to={'/'}
-            className={({ isActive }) =>
-              isActive
-                ? `${styles.navLink} ${styles.navLink__active}`
-                : styles.navLink
-            }
-          >
-            Following
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to={'/likes'}
-            className={({ isActive }) =>
-              isActive
-                ? `${styles.navLink} ${styles.navLink__active}`
-                : styles.navLink
-            }
-          >
-            Likes
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
+      <Link
+        onClick={() => window.location.reload()}
+        to={'/'}
+        className={styles.logoLink}
+      >
+        Rumblr
+      </Link>
+
+      <nav aria-label="topBar" className={styles.topBar__nav}>
+        <HeaderLinks />
+      </nav>
+
+      <div className={styles['searchWrapper--noMobile']}>
+        <SearchFrom />
+      </div>
+    </div>
   );
 }
 
