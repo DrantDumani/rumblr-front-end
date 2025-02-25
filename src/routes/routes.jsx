aa902cce6a6d03edce3e77396ca653b6af03d268
Darnell <darnjames@gmail.com>
2025-02-25 16:04:53 -0500

    Add error element to routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -18,46 +18,53 @@ import { Blog } from '../pages/blog/Blog';
 import { Users } from '../pages/users/Users';
 import { Search } from '../pages/search/Search';
 import { PostPage } from '../pages/post/PostPage';
+import { Error } from '../pages/error/Error';
 
 export const routes = [
   {
     element: <Dashboard />,
     path: '/',
+    errorElement: <Error />,
     children: [
       {
-        index: true,
-        element: <Index />,
-        loader: dashboardLoader,
-      },
-      {
-        path: '/settings',
-        element: <Settings />,
-        loader: settingsLoader,
-      },
-      {
-        path: '/likes',
-        element: <Likes />,
-        loader: likesLoader,
-      },
-      {
-        path: '/blog/:userId',
-        element: <Blog />,
-        loader: blogLoader,
-      },
-      {
-        path: '/post/:postId',
-        element: <PostPage />,
-        loader: singlePostLoader,
-      },
-      {
-        path: '/users',
-        element: <Users />,
-        loader: usersLoader,
-      },
-      {
-        path: '/search',
-        element: <Search />,
-        loader: searchLoader,
+        errorElement: <Error />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+            loader: dashboardLoader,
+          },
+          {
+            path: '/settings',
+            element: <Settings />,
+            loader: settingsLoader,
+          },
+          {
+            path: '/likes',
+            element: <Likes />,
+            loader: likesLoader,
+          },
+          {
+            path: '/blog/:userId',
+            element: <Blog />,
+            loader: blogLoader,
+          },
+          {
+            path: '/post/:postId',
+            element: <PostPage />,
+            loader: singlePostLoader,
+          },
+          {
+            path: '/users',
+            element: <Users />,
+            loader: usersLoader,
+          },
+          {
+            path: '/search',
+            element: <Search />,
+            loader: searchLoader,
+          },
+        ],
       },
     ],
   },
